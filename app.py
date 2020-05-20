2b534bb1e1be6fd37651a5719477f9bd77726a32
xenoveals <xenoveals@gmail.com>
2020-05-20 11:55:35 +0700

    register and login fix bug, add google login/register

--- app.py
+++ app.py
@@ -1,68 +1,135 @@
 from flask import Flask, session, render_template, request, url_for, redirect
 from flask_session import Session
+from authlib.integrations.flask_client import OAuth
 from models import *
+from hashPassword import *
+import time
 
 app = Flask(__name__)
 
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
 # Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
+app.config["PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://fgaelkvcnxhqos:7784fabbd6c0316eb419e127eeb3e50ac6e5808fcc827204fe982cffe88b2a3c@ec2-52-44-55-63.compute-1.amazonaws.com:5432/d7s2do0pifj2qc"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+# Google login
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id="630158134949-pd5grh25vtme67brml89a69n5a5qo382.apps.googleusercontent.com",
+    client_secret="IAwl2pMU7OUQl3CZ_Vt25qxC",
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 data = None
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def index():
     title = "Home"
     if(data == None):
         return render_template("index.html", title=title, login=False)
-    return render_template("index.html", title=title, login=True)
+    return render_template("index.html", title=title, login=True, name=data['First Name'][0].capitalize()+data['First Name'][1:])
 
+@app.route("/google-login")
+def login_google():
+    google = oauth.create_client('google')  # create the google oauth client
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
 
-@app.route("/login", methods=["POST"])
-def login():
-    username_fill = request.form.get("loginEmail")
-    password_fill = request.form.get("loginPass")
-    user = User.query.filter_by(username=username_fill).first()
-
-    if(not(user is None)):
-        if(user.password==password_fill):
-            global data 
-            data = {
-                'Email': user.username,
-                'First Name' : user.first_name,
-                'Last Name' : user.last_name
-            }
-    return redirect(url_for('.index'))
-
-@app.route("/register", methods=["POST"])
-def register():
-    username = request.form.get("registerEmail")
-    password = request.form.get("registerPass")
-    password2 = request.form.get("registerPass2")
-    if(password==password2):
-        new_user = User(username=username, password=password)
+@app.route('/authorize')
+def authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    global data
+    data = {
+        'Email' : user_info['email'],
+        'First Name' : user_info['given_name'],
+        'Last Name' : user_info['family_name']
+    }    
+    user = User.query.filter_by(username=data['Email']).first()
+    
+    # if user is new, add to database with default PASSWORD = admin1234
+    if(user is None):
+        new_user = User(username=data['Email'], password=hash_password('admin1234'), first_name=data['First Name'], last_name=data['Last Name'])
         db.session.add(new_user)
         try:
             db.session.commit()
         except:
             db.session.rollback()
+    else:
+        data = {
+            'Email': user.username,
+            'First Name': user.first_name,
+            'Last Name': user.last_name
+        } 
+
+    time.sleep(1)  
     return redirect(url_for('.index'))
 
-@app.route("/logout")
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    title = "Login"
+    if(request.method=="POST"):
+        username_fill = request.form.get("loginEmail")
+        password_fill = request.form.get("loginPass")
+        user = User.query.filter_by(username=username_fill).first()
+
+        if(not(user is None)):
+            if(verify_password(user.password, password_fill)):
+                global data 
+                data = {
+                    'Email': user.username,
+                    'First Name' : user.first_name,
+                    'Last Name' : user.last_name
+                }
+                time.sleep(1)
+                return redirect(url_for('.index'))
+        return redirect(url_for('.login', wrong=True))
+    else:
+        if(data!=None):
+            return redirect(url_for('.index'))
+        wrong = request.args.get('wrong')
+        return render_template("login.html", title=title, wrong=wrong)
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    title="Register"
+    if(request.method=="POST"):
+        username = request.form.get("registerEmail")
+        password = request.form.get("registerPass")
+        password2 = request.form.get("registerPass2")
+        if(password==password2):
+            first_name = username.split('@')[0]
+            new_user = User(username=username, password=hash_password(password), first_name=first_name)
+            db.session.add(new_user)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+        time.sleep(1)
+        return redirect(url_for('.index'))
+    else:
+        if(data!=None):
+            return redirect(url_for('.index'))
+        return render_template("register.html", title=title)
+
+@app.route("/l1o1g1o1u1t")
 def logout():
     global data
     data = None
+    time.sleep(1)
     return redirect(url_for('.index'))
 
 @app.route("/blog")
--- static/js/script.js
+++ static/js/script.js
@@ -1,113 +1,57 @@
-(function ($) {
-	$.fn.countTo = function (options) {
-		options = options || {};
-		
-		return $(this).each(function () {
-			// set options for current element
-			var settings = $.extend({}, $.fn.countTo.defaults, {
-				from:            $(this).data('from'),
-				to:              $(this).data('to'),
-				speed:           $(this).data('speed'),
-				refreshInterval: $(this).data('refresh-interval'),
-				decimals:        $(this).data('decimals')
-			}, options);
-			
-			// how many times to update the value, and how much to increment the value on each update
-			var loops = Math.ceil(settings.speed / settings.refreshInterval),
-				increment = (settings.to - settings.from) / loops;
-			
-			// references & variables that will change with each update
-			var self = this,
-				$self = $(this),
-				loopCount = 0,
-				value = settings.from,
-				data = $self.data('countTo') || {};
-			
-			$self.data('countTo', data);
-			
-			// if an existing interval can be found, clear it first
-			if (data.interval) {
-				clearInterval(data.interval);
-			}
-			data.interval = setInterval(updateTimer, settings.refreshInterval);
-			
-			// initialize the element with the starting value
-			render(value);
-			
-			function updateTimer() {
-				value += increment;
-				loopCount++;
-				
-				render(value);
-				
-				if (typeof(settings.onUpdate) == 'function') {
-					settings.onUpdate.call(self, value);
-				}
-				
-				if (loopCount >= loops) {
-					// remove the interval
-					$self.removeData('countTo');
-					clearInterval(data.interval);
-					value = settings.to;
-					
-					if (typeof(settings.onComplete) == 'function') {
-						settings.onComplete.call(self, value);
-					}
-				}
-			}
-			
-			function render(value) {
-				var formattedValue = settings.formatter.call(self, value, settings);
-				$self.html(formattedValue);
-			}
-		});
-	};
-	
-	$.fn.countTo.defaults = {
-		from: 0,               // the number the element should start at
-		to: 0,                 // the number the element should end at
-		speed: 1000,           // how long it should take to count between the target numbers
-		refreshInterval: 100,  // how often the element should be updated
-		decimals: 0,           // the number of decimal places to show
-		formatter: formatter,  // handler for formatting the value before rendering
-		onUpdate: null,        // callback method for every time the element is updated
-		onComplete: null       // callback method for when the element finishes updating
-	};
-	
-	function formatter(value, settings) {
-		return value.toFixed(settings.decimals);
-	}
-}(jQuery));
-
-jQuery(function ($) {
-  // custom formatting example
-  $('.count-number').data('countToOptions', {
-	formatter: function (value, options) {
-	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-	}
-  });
-  
-  // start all the timers
-  $('.timer').each(count);  
-  
-  function count(options) {
-	var $this = $(this);
-	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-	$this.countTo(options);
-  }
-});
-
 var allowSubmit = false;
-$('#registerPass, #registerPass2').on('keyup', function(){
-	if($('#registerPass').val() != $('#registerPass2').val()){
-		$('#registerPass').addClass('is-invalid');
-		$('#registerPass2').addClass('is-invalid');
-		$('#msg').html('Password didn\'t Match!');
+$('#registerEmail').on('input', function(){
+	var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	var is_email=re.test($('#registerEmail').val());
+
+	if(($('#registerEmail').val().length > 5) && (is_email)){
+		$('#registerEmail').removeClass('is-invalid').addClass('is-valid')
+		$('#msg2').html('');
 	}else{
-		$('#msg').html('');
-		$('#registerPass').toggleClass('is-invalid is-valid');
-		$('#registerPass2').toggleClass('is-invalid is-valid');
-		allowSubmit = true;
+		$('#registerEmail').removeClass('is-valid').addClass('is-invalid');
+		$('#msg2').html('Give a valid email!');
 	}
 });
 
+$('#registerPass, #registerPass2').on('input', function(){
+	if(($('#registerPass').val().length > 7) && (/\d/.test($('#registerPass').val()))){
+		if($('#registerPass').val() != $('#registerPass2').val()){
+			$('#registerPass2').addClass('is-invalid');
+			$('#msg').html('Password didn\'t Match!');
+		}else{
+			$('#msg').html('');
+			$('#registerPass').toggleClass('is-invalid is-valid');
+			$('#registerPass2').toggleClass('is-invalid is-valid');
+			allowSubmit = true;
+		}
+	}else{
+		$('#registerPass').addClass('is-invalid');
+		$('#msg').html('Password minimum 8 characters with numeric values');
+	}
+});
+
+$("#submit-register").click(function(event){
+	var form_data=$("#register").serializeArray();
+	var error_free=true;
+	for (var index in form_data){
+		var element=$("#"+form_data[index]['name']);
+		var isnotvalid=element.hasClass("is-invalid");
+		var element_value=element.val()
+		if (isnotvalid || !element_value){error_free=false;}
+	}
+	var agree_check=$("#agreePolicy").is(":checked")
+	if(!agree_check){error_free=false}
+	if (!error_free){
+		event.preventDefault(); 
+	}
+});
+
+$('#registerPass, #registerPass2').keypress(function(e) { 
+	var s = String.fromCharCode( e.which );
+
+	if((s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey) ||
+	   (s.toUpperCase() !== s && s.toLowerCase() === s && e.shiftKey)){
+		$('#capsWarning').html('Capslock is ON!');
+	} else {
+		$('#capsWarning').html('');
+	}
+});
